e65f5b28a40078f4b31df17133b6519a0fc8da94
mattj241 <londonm241@gmail.com>
2019-02-15 21:49:03 -0500

    Logic and comments complete

--- Slots/app/src/main/java/com/android/london/slotmachine/MainActivity.java
+++ Slots/app/src/main/java/com/android/london/slotmachine/MainActivity.java
@@ -8,11 +8,15 @@ import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
+/*
+* Author: Matthew London
+* Professor: John P. Baugh
+* Class: CIS 436
+* Project: Slot Machine - Project 1
+* */
 public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     //Global variables
@@ -20,7 +24,11 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     static String OutOfBank = "You ran out of money, you are not lucky.";
     static String NewGame = "New Game Started.";
     static int newGameBankAmount = 0;
-    static int bound = 8;
+    static int randomBound = 8;
+    static int smallPrize = 10;
+    static int midPrize = 40;
+    static int bigPrize = 100;
+    static int grandPrize = 1000;
     int currentBank;
 
     //Widgets
@@ -36,6 +44,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Creating identifiers for changing UI components
         setValueBtn = findViewById(R.id.SetValueBtn);
         pullLeverBtn = findViewById(R.id.PullBtn);
         newGameBtn = findViewById(R.id.NewGameBtn);
@@ -52,6 +61,8 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     @Override
     public void onClick(View v){
+
+        //Switch case that calls the handler of the button caller
         switch (v.getId()){
             case R.id.SetValueBtn:
                 setValueHandler();
@@ -67,6 +78,9 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         }
     }
 
+    //Pre: The "Set Value" button must have been pressed by its onClick listener
+    //Post: Rejects the user if the initial value is within bounds,
+    //      otherwise posts the input to the bank and allows the lever button to be pulled
     private void setValueHandler(){
         int amount;
         String input = setValueAmount.getText().toString();
@@ -80,10 +94,14 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
             currentBank = amount;
             updateBankText(currentBank);
             setValueBtn.setClickable(false);
+            pullLeverBtn.setClickable(true);
             setValueAmount.setEnabled(false);
         }
     }
 
+    //Pre: The "New Game" button must have been pressed by its onClick listener,
+    //     or the user's bank falls below 0 or above 1000
+    //Post: Disables the pull lever btn and re-enables the bank input dialogue
     private void newGameHandler(boolean isOutOfBank){
         if (isOutOfBank) {
             Toast.makeText(MainActivity.this, OutOfBank , Toast.LENGTH_LONG).show();
@@ -91,45 +109,100 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         else {
             Toast.makeText(MainActivity.this, NewGame , Toast.LENGTH_LONG).show();
         }
+        setValueAmount.setText("");
         currentBank = newGameBankAmount;
+        updateBankText(currentBank);
         setValueBtn.setClickable(true);
+        pullLeverBtn.setClickable(false);
         setValueAmount.setEnabled(true);
     }
 
+    //Pre: The "Pull Lever" button must have been pressed by its onClick listener
+    //Post: An iteration of the slot machine game is completed,
+    //      -5 from the bank per pull and rewards are calculated in another function
     private void slotsHandler(){
         int numOne, numTwo, numThree;
         Random rnd = new Random();
-        numOne = rnd.nextInt(bound);
+        numOne = rnd.nextInt(randomBound);
         numOne++;
-        numTwo = rnd.nextInt(bound);
+        numTwo = rnd.nextInt(randomBound);
         numTwo++;
-        numThree = rnd.nextInt(bound);
+        numThree = rnd.nextInt(randomBound);
         numThree++;
-
-        currentBank -= 5;
-        currentBank = checkForMatches(numOne, numTwo, numThree, currentBank);
-        updateBankText(currentBank);
-        if (currentBank == 0){
-            newGameHandler(true);
-        }
-
 
         slot1.setText(String.format("%s", numOne));
         slot2.setText(String.format("%s", numTwo));
         slot3.setText(String.format("%s", numThree));
+
+        currentBank -= 5;
+        currentBank = checkForMatches(numOne, numTwo, numThree, currentBank);
+        updateBankText(currentBank);
+
+        //New Game logic implementation check after every pull
+        if (currentBank == 0){
+            newGameHandler(true);
+        }
+        else if (currentBank >= 1000){
+            Toast.makeText(MainActivity.this, "Machine is cleared out!" , Toast.LENGTH_SHORT).show();
+            newGameHandler(false);
+        }
     }
 
+    //Pre: A decent programmer (haha) placed these function calls after all executions that require an update to the bank status
+    //     Examples include: Set Value button press, new game button press, and pull lever button press
+    //Post: Bank status updated
     private void updateBankText(int amount){
         bankAmount.setText(String.format("%s", amount));
     }
 
+    //Pre: Pull lever button was pulled and this function was called by the main slot machine handler
+    //Post: All logic is sorted and rewards the user based on number of random matches in the slot machine
     private int checkForMatches(int slot1, int slot2, int slot3, int inputBank){
-        int matchCounter = 0;
+        int highestMatchCounter = 0;
+        int tempCounter = 0;
         ArrayList<Integer> matchList = new ArrayList<Integer>();
 
+        //add all slots to a list for easier iteration
         matchList.add(slot1);
         matchList.add(slot2);
         matchList.add(slot3);
+
+        //Check all number slots for identical matches in other slots...doesn't seem efficient, thoughts on how to improve?
+        for (int i = 0; i < matchList.size(); i++){
+            for (int j = 0; j < matchList.size(); j++){
+                if (matchList.get(i) == matchList.get(j)){
+                    tempCounter++;
+                    highestMatchCounter = (tempCounter > highestMatchCounter) ? tempCounter : highestMatchCounter;
+                }
+            }
+            tempCounter = 0;
+        }
+
+
+        //If-blocks below decide on the prize money based of the highest match
+        if (highestMatchCounter == 2){
+            //Toast.makeText(MainActivity.this, "Small Prize Awarded!" , Toast.LENGTH_SHORT).show();
+            return inputBank += smallPrize;
+        }
+
+        else if (highestMatchCounter == 3){
+            if (matchList.get(0) < 5){
+                //Toast.makeText(MainActivity.this, "Mid Prize Awarded!" , Toast.LENGTH_SHORT).show();
+                return inputBank += midPrize;
+            }
+            else if (matchList.get(0) >= 5 && matchList.get(0) <= 8){
+                //Toast.makeText(MainActivity.this, "Big Prize Awarded!" , Toast.LENGTH_SHORT).show();
+                return inputBank += bigPrize;
+            }
+            else {
+                //Toast.makeText(MainActivity.this, "Grand Prize Awarded!" , Toast.LENGTH_SHORT).show();
+                return inputBank += grandPrize;
+            }
+        }
+
+        else {
+            return inputBank;
+        }
     }
 
 
